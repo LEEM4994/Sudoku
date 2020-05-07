802cec0fca70601b172a70e717f5bb6fddd61fc1
CastilloCanedoMichel <CastilloCanedoMichel@WINDOWS-Q7F3BK8>
2020-05-07 01:22:11 -0500

    Passed the array to the other class

--- src/application/SudokuApp.java
+++ src/application/SudokuApp.java
@@ -11,6 +11,8 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
@@ -28,7 +30,7 @@ import javafx.stage.Stage;
  *
  */
 public class SudokuApp extends Application {
-	
+
 	private TextField sudokuGrid[][];
 	private Stage window;
 	private Font font;
@@ -41,14 +43,16 @@ public class SudokuApp extends Application {
 	private AudioFormat format;
 	private Clip soundClip;
 	private DataLine.Info data;
+	private ChangeListener box;
 
 	@Override
 	public void start(Stage window) throws Exception {
-		font = new Font ("Arial", 40);
-		text = new Text("Welcome to Sudoku!\n\nPress the S key to play.\n\nClick on spaces and enter\nnumbers.\n\nPress escape to quit.");
+		font = new Font("Arial", 40);
+		text = new Text(
+				"Welcome to Sudoku!\n\nPress the S key to play.\n\nClick on spaces and enter\nnumbers.\n\nPress escape to quit.");
 		text.setFont(font);
-		pane = new FlowPane (text);
-		scene1 = new Scene (pane, 600, 400, Color.WHITESMOKE);
+		pane = new FlowPane(text);
+		scene1 = new Scene(pane, 600, 400, Color.WHITESMOKE);
 		this.window = window;
 		scene1.setOnKeyPressed(this::processKeyPress);
 		this.window.setTitle("Wack-A-Mole");
@@ -56,25 +60,34 @@ public class SudokuApp extends Application {
 		this.window.show();
 
 	}
-	
+
 	private void processKeyPress(KeyEvent event) {
-		if(event.getCode() == KeyCode.S) {
-			sudokuGrid = new TextField [9][9];
-			//Populate TextField
-			for(int i = 0; i < 9; i++) {
-				for(int j = 0; j < 9; j++) {
-					root = new Group (sudokuGrid[i][j]);
+		if (event.getCode() == KeyCode.S) {
+			sudokuGrid = new TextField[9][9];
+			initializeGrid(sudokuGrid);
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					root = new Group(sudokuGrid[i][j]);
 				}
 			}
-			
-			Scene scene2 = new Scene (root, 750, 750, Color.GHOSTWHITE);
+
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sudokuGrid[i][j].setOnMouseClicked(this::processMouseClick);
+				}
+			}
+
+			Scene scene2 = new Scene(root, 750, 750, Color.GHOSTWHITE);
 			this.window.setScene(scene2);
-		}
-		else if(event.getCode() == KeyCode.ESCAPE) {
+		} else if (event.getCode() == KeyCode.ESCAPE) {
 			text.setText("Thanks for playing!");
 			this.window.setScene(scene1);
-			//soundClip.stop();
+			// soundClip.stop();
 		}
+	}
+
+	void processMouseClick(MouseEvent event) {
+		
 	}
 
 	public static void main(String[] args) {
